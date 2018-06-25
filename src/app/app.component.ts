a53f4ae116711054497aa26a983e95f7c9294641
th0r <grunin.ya@ya.ru>
2018-06-25 20:57:29 +0300

    Add `mobx-angular`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { observable, action } from 'mobx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'app';
+  @observable title = 'Click `Change title` button to ensure that app works';
+
+  @action changeTitle() {
+    this.title = 'New title';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MobxAngularModule } from 'mobx-angular';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MobxAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
